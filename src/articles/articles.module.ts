ad8ecb90573174b8a36e8cd7a866f99e89d403f0
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2023-12-16 14:36:44 +0900

    feat: ✨Add PrismaClient to the Articles module

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { ArticlesController } from './articles.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
   controllers: [ArticlesController],
   providers: [ArticlesService],
+  /*
+  Articles モジュール内で PrismaClient にアクセスするには
+  PrismaModule を import として追加する
+  PrismaModule は PrismaService を export していて、
+  PrismaServiceは PrismaClient を拡張している
+  */
+  imports: [PrismaModule],
 })
 export class ArticlesModule {}
--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -3,6 +3,11 @@ import { PrismaService } from './prisma.service';
 
 @Module({
   providers: [PrismaService],
+  /*
+PrismaModule をインポートしたモジュールは PrismaService にアクセスできるようになり、
+独自のコンポーネント/サービスにインジェクトできるようになります。
+PrismaService は PrismaClient を拡張している
+ */
   exports: [PrismaService],
 })
 export class PrismaModule {}
